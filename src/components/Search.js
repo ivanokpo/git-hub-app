4c543f9fb6ed3fcf95b402e754d94d99f930df9f
ivan o <ivanokpomor@gmail.com>
2023-07-27 22:04:08 +0100

    feat: search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,18 +3,21 @@ import TextField from '@mui/material/TextField';
 import axios from 'axios';
 import IconButton from '@mui/material/IconButton';
 import { AiOutlineSearch } from "react-icons/ai";
+import Paper from '@mui/material/Paper';
+import Results from "./Results";
 
 const Search = () => {
     //state for input in searchbar
     const [input, setInput] = useState("");
+    //state for fetched data
     const [repos, setRepos] = useState([]);
     
   
     //method for when the search bar is pressed, triggering a search in the database
     const onSearch = (e) => {
     const value = e.target.value
-      const searchInput = value.toLowerCase();
-      setInput(searchInput);
+      
+      setInput(value.toLowerCase());
       console.log(input);
       
     };
@@ -25,12 +28,16 @@ const Search = () => {
             url: `https://api.github.com/users/${input}/repos`
         }).then(res => {
             setRepos(res.data)
-            console.log(repos.name)
+            console.log(repos)
         })
     }
   
     return (
       <>
+      <Paper
+      component="form"
+      sx={{ display: 'flex', alignItems: 'center', width: 400 }}
+    >
         <TextField 
         label="Search Git Repository"
         color="secondary"
@@ -39,9 +46,9 @@ const Search = () => {
         <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={() => searchGithub()}>
         <AiOutlineSearch/>
         </IconButton>
-          
-        
-      
+        </Paper>
+
+        <Results data={repos}/>
       </>
     );
   };
